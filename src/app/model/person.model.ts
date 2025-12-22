ee90127f99d344eec4d11dbd37abd507b4fc54f7
Masoud Bimmar <masoudbimar@gmail.com>
2025-12-22 23:53:43 +0100

    Angular Step By Step (v14) -- Get Started

---
+++ src/app/model/person.model.ts
@@ -0,0 +1,10 @@
+export class Person {
+  public fn!: string;
+  public ln!: string;
+  public img!: string;
+  public birthDate!: Date;
+  public dsc!: string;
+  public summary!: string;
+  public type!: string;
+
+}
