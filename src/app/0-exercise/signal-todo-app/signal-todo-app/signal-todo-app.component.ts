bd43a9022074d44a9ba719e98db571eac3f07640
Masoud Bimmar <masoudbimar@gmail.com>
2026-01-19 00:08:34 +0100

    Update the style and clean code in signal-todo-app

--- src/app/0-exercise/signal-todo-app/signal-todo-app/signal-todo-app.component.ts
+++ src/app/0-exercise/signal-todo-app/signal-todo-app/signal-todo-app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TodoItem } from '../model/to-do-item.model';
 import { TodoItemService } from '../service/todo-item.service';
@@ -11,44 +11,45 @@ import { TodoItemService } from '../service/todo-item.service';
   templateUrl: './signal-todo-app.component.html',
   styleUrl: './signal-todo-app.component.scss'
 })
-export class SignalTodoAppComponent implements OnInit {
-  protected readonly list = signal<TodoItem[]>([]);
+export class SignalTodoAppComponent {
   private readonly todoItemService = inject(TodoItemService);
+  protected readonly list: WritableSignal<TodoItem[]> = signal<TodoItem[]>(
+    this.todoItemService.getItems() || []
+  );
+  protected readonly openItems = computed(() =>
+    this.list().filter((item) => !item.isComplete)
+  );
+  protected readonly doneItems = computed(() =>
+    this.list().filter((item) => item.isComplete)
+  );
 
-  protected openItems = computed(() => {
-    let items = this.list();
-    if (items.length > 0) {
-      return items.filter(item => !item.isComplete);
-    } else {
-      return [];
+  addNewItem(input: HTMLInputElement) {
+    const trimmedItem = input.value.trim();
+    if (trimmedItem) {
+      this.list.update((items: TodoItem[]) => [new TodoItem(trimmedItem), ...items]);
     }
-  });
 
-  protected doneItems = computed(() => {
-    let items = this.list();
-    if (items.length > 0) {
-      return items.filter(item => item.isComplete);
-    } else {
-      return [];
-    }
-  });
-
-  ngOnInit(): void {
-    this.list.set(this.todoItemService.getItems());
+    input.value = '';
+    input.focus();
   }
 
-  addNewItem(text: string) {
-    console.log(text);
-    const trimmedItem = text.trim();
-    if (trimmedItem) {
-      this.list.update((items: TodoItem[]) => [...items, new TodoItem(trimmedItem)]);
+  removeItem(item: TodoItem) {
+    if (item) {
+      this.list.update((items: TodoItem[]) =>
+        items.filter((current) => current.id !== item.id)
+      );
     }
   }
 
   toggleItemStatus(taskItem: TodoItem): void {
     this.list.update((items: TodoItem[]) =>
-      items.map(item => item.id === taskItem.id ? { ...item, isComplete: !item.isComplete } : item)
-    )
+      items.map((item) =>
+        item.id === taskItem.id ? { ...item, isComplete: !item.isComplete } : item
+      )
+    );
   }
 
+  trackById(index: number, item: TodoItem) {
+    return item.id;
+  }
 }
