b0c81acacc3373d949553ee8c0a6b12e7891a904
Masoud Bimmar <masoudbimar@gmail.com>
2026-01-24 21:43:11 +0100

    Update Code section inside con-tags

--- src/app/10-component-lifecycles/lifecycles/lifecycles.component.ts
+++ src/app/10-component-lifecycles/lifecycles/lifecycles.component.ts
@@ -11,6 +11,46 @@ import { Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges 
 export class LifecyclesComponent implements OnInit, OnDestroy, DoCheck, OnChanges {
   @Input() inputs!: any;
 
+  readonly onChangesCode = `ngOnChanges(changes: SimpleChanges) {
+  if (changes['userId']) {
+    this.loadUser(changes['userId'].currentValue);
+  }
+}`;
+
+  readonly onInitCode = `ngOnInit() {
+  this.status = 'ready';
+  this.loadDashboard();
+  this.sub = this.userService.user$.subscribe();
+}`;
+
+  readonly doCheckCode = `ngDoCheck() {
+  if (this.items.length !== this.prevCount) {
+    this.prevCount = this.items.length;
+    this.recalculateTotals();
+  }
+}`;
+
+  readonly afterContentInitCode = `ngAfterContentInit() {
+  this.projectedTitle = this.contentTitle?.text;
+}`;
+
+  readonly afterContentCheckedCode = `ngAfterContentChecked() {
+  this.syncProjectedState();
+}`;
+
+  readonly afterViewInitCode = `ngAfterViewInit() {
+  this.searchInput?.nativeElement.focus();
+}`;
+
+  readonly afterViewCheckedCode = `ngAfterViewChecked() {
+  this.alignTooltip();
+}`;
+
+  readonly onDestroyCode = `ngOnDestroy() {
+  this.sub?.unsubscribe();
+  clearInterval(this.timerId);
+}`;
+
   ngOnInit(): void {
     console.log('onInit called');
   }
--- src/app/11-decorators/decorators.component.ts
+++ src/app/11-decorators/decorators.component.ts
@@ -1,13 +1,100 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   standalone: true,
   imports: [],
   selector: 'app-decorators',
   templateUrl: './decorators.component.html',
-  styleUrls: ['./decorators.component.scss']
+  styleUrls: ['./decorators.component.scss'],
 })
 export class DecoratorsComponent {
+  readonly componentCode = `@Component({
+  selector: 'app-profile-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profile-card.component.html',
+  styleUrl: './profile-card.component.scss',
+})
+export class ProfileCardComponent {}`;
 
+  readonly directiveCode = `@Directive({
+  selector: '[appAutoFocus]',
+  standalone: true,
+})
+export class AutoFocusDirective {
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.el.nativeElement.focus();
+  }
+}`;
+
+  readonly pipeCode = `@Pipe({ name: 'initials', standalone: true })
+export class InitialsPipe implements PipeTransform {
+  transform(value: string) {
+    return value
+      .split(' ')
+      .map((part) => part[0])
+      .join('');
+  }
+}`;
+
+  readonly injectableCode = `@Injectable({ providedIn: 'root' })
+export class MovieService {
+  getAll() {
+    return this.http.get('/api/movies');
+  }
+
+  constructor(private http: HttpClient) {}
+}`;
+
+  readonly ngModuleCode = `@NgModule({
+  declarations: [LegacyCardComponent],
+  imports: [CommonModule],
+  exports: [LegacyCardComponent],
+})
+export class SharedModule {}`;
+
+  readonly inputOutputCode = `export class RatingBadgeComponent {
+  @Input({ required: true }) value = 0;
+  @Output() valueChange = new EventEmitter();
+
+  update(next: number) {
+    this.value = next;
+    this.valueChange.emit(next);
+  }
+}`;
+
+  readonly viewChildCode = `@ViewChild('search') searchInput!: ElementRef;
+@ViewChildren(ItemCardComponent) cards!: QueryList;
+
+ngAfterViewInit() {
+  this.searchInput.nativeElement.focus();
+}`;
+
+  readonly contentChildCode = `@ContentChild('title') titleTpl?: TemplateRef;
+@ContentChildren(TabComponent) tabs!: QueryList;
+
+ngAfterContentInit() {
+  this.activeTab = this.tabs.first;
+}`;
+
+  readonly hostBindingCode = `@HostBinding('class.is-active') isActive = false;
+
+@HostListener('mouseenter')
+onEnter() {
+  this.isActive = true;
+}
+
+@HostListener('mouseleave')
+onLeave() {
+  this.isActive = false;
+}`;
+
+  readonly diParameterCode = `constructor(
+  @Optional() @Inject(API_URL) apiUrl: string | null,
+  @SkipSelf() private parent: ParentService,
+  @Self() private local: LocalService,
+  @Host() private host: HostService
+) {}`;
 }
